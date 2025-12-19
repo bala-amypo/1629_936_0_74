fd476d81f588e65433d43c7b0d9e5ec8e3c67235
bala-amypo <bala@amypo.in>
2025-12-19 08:17:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/TimestampController.java
+++ src/main/java/com/example/demo/controller/TimestampController.java
@@ -4,10 +4,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import jakarta.validation.Valid;
-
 import com.example.demo.entity.Timestamp;
 import com.example.demo.service.TimestampService;
 
@@ -18,7 +14,7 @@ public class TimestampController{
     @Autowired TimestampService TR;
     
     @PostMapping("/posting")
-    public Timestamp postData(@Valid @RequestBody Timestamp t){
+    public Timestamp postData(@RequestBody Timestamp t){
         return valid.postValue1(t);
     }
 }
--- src/main/java/com/example/demo/service/Impl/TimestampServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimestampServiceImpl.java
@@ -1,11 +1,10 @@
 package com.example.demo.service.impl;
+
 import org.springframework.stereotype.Service;
 import com.example.demo.service.TimestampService;
 import com.example.demo.entity.Timestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.repository.TimestampRepo;
-
-
 
 @Service
 public class TimestampServiceImpl implements TimestampService{
