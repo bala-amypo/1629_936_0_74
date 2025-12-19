939548597cb383b1d212a97624a7a8858a5e6d11
bala-amypo <bala@amypo.in>
2025-12-19 06:11:42 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalException.java
+++ src/main/java/com/example/demo/exception/GlobalException.java
@@ -2,7 +2,7 @@ package com.example.exception;
 
 import org.springframework.web.bind.annotation.RestControlleAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 @RestControllerAdvice
 public class GlobalException{
