766daf6ec8f45ba01d799a2e0d7afb620473e4c5
bala-amypo <bala@amypo.in>
2025-12-19 10:32:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/IdEntity.java
+++ src/main/java/com/example/demo/entity/IdEntity.java
@@ -1,26 +1,26 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
-// import lombok.AllArgsConstructor;
-// import lombok.NoArgsConstructor;
-// import lombok.Data;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.OneToOne;
-// import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
 
-// @Entity
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
-// public class IdEntity{
-//      @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Integer id;
-//     private int cardNo;
-// //     private int student_id;
-//     @OneToOne
-//     @JoinColumn(name="student_id")
-//     private stuEntity stu;
-//     }
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IdEntity{
+     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    private int cardNo;
+//     private int student_id;
+    @OneToOne
+    @JoinColumn(name="student_id")
+    private stuEntity stu;
+    
